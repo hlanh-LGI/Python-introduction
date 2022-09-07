03c49a5a16550916746a4815ff29313b30c2d1bb
hlanh-LGI <hlanh.20082K3@gmail.com>
2022-09-08 00:18:06 +0700

    update test_python file

--- test_python.py
+++ test_python.py
@@ -1,10 +1,29 @@
-number =int(input('input of number:'))
-"""erro: > not support str and int"""   
-print(number) 
-sum=0     
-while (number > 10):
-    sum=sum+number%10
-    number=int(number/10)
-    sum=sum+number
-print(sum)
+# TODO: 
+"""
+1. 
+2.
+3.
 
+"""
+
+# DEBUG: 
+"""error: > not support str and int"""   
+
+
+# print(f'number: {number}')  
+
+# name = 'long'
+# print('number: {}, name: {}'.format(number, name))
+
+def sum_number(number):
+    sum = 0 
+    while (number > 10): 
+        sum=sum+number%10
+        number=int(number/10)
+    sum=sum+number
+    return sum
+
+if __name__ == "__main__":
+    number =int(input('Input number: '))
+    print("number:",number) 
+    print(sum_number(number))
